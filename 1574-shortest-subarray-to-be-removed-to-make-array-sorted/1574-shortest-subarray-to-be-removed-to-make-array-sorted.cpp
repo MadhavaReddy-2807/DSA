3ed2a67d3948e57b97c3c804088cc05e55a68c73
MadhavaReddy-2807 <madhava2807@gmail.com>
2025-07-23 17:47:30 +0530

    Time: 105 ms (5.25%), Space: 70.8 MB (8.35%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -13,12 +13,13 @@ public:
             return 0;
         }
         int res=j;
-        while(i<=j&&j<arr.size()&&(i==0||arr[i]>=arr[i-1]))
+        while(i<j&&(i==0||arr[i]>=arr[i-1]))
         {
             while(j<arr.size()&&(arr[i]>arr[j]))
             {
                 j++;
             }
+            cout<<i<<j<<endl;
             res=min(res,j-i-1);
             i++;
         }
