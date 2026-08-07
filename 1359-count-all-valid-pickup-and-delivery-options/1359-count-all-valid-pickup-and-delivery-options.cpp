515879d2c13ef039ae771253754d097fc5f19f8f
Madhava_Reddy <madhava2807@gmail.com>
2026-08-07 17:36:33 +0530

    Time: 0 ms (100%), Space: 8.8 MB (24.2%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+int mod=1e9+7;
+    int countOrders(int n) {
+        vector<long long>dp(n+1,0);
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=(dp[i-1]*(2*i-1)*i)%mod;
+        }
+        return dp[n];
+    }
+};
