76d737b825927f76d5a3196d63f3ef8033552b34
Madhava_Reddy <madhava2807@gmail.com>
2026-08-11 18:06:12 +0530

    Time: 1096 ms (5.35%), Space: 352.5 MB (5.1%) - LeetHub

--- 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.cpp
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.cpp
@@ -1,83 +1,80 @@
+class node{
+    public:
+    int val;
+    node*left;
+    node*right;
+};
+class st{
+    public:
+    node*root;
+    st()
+    {
+        root=new node();
+    }
+    void build(node*root,int l,int r,vector<int>&nums)
+    {
+        if(l==r)
+        {
+            root->val=nums[l];
+            return;
+        }
+        root->left=new node();
+        root->right=new node();
+        int mid=(l+r)/2;
+        build(root->left,l,mid,nums);
+        build(root->right,mid+1,r,nums);
+        root->val=max(root->left->val,root->right->val);
+    }
+    int query(node*root,int nl,int nr,int l,int r)
+    {
+        if(l>nr||r<nl)
+        {
+            return INT_MIN;
+        }
+        if(l<=nl&&nr<=r)
+        {
+            return root->val;
+        }
+        int mid=(nl+nr)/2;
+        int left=query(root->left,nl,mid,l,r);
+        int right=query(root->right,mid+1,nr,l,r);
+        return max(left,right);
+    }
+};
 class Solution {
 public:
-vector<int>seg;
-void build(int index,int l,int r,vector<int>heights)
-{
-    if(l==r)
-    {
-        seg[index]=l;
-        return;
-    }
-    int mid=(l+r)/2;
-    build(2*index+1,l,mid,heights);
-    build(2*index+2,mid+1,r,heights);
-    int left=seg[2*index+1];
-    int right=seg[2*index+2];
-    if(heights[left]>=heights[right])
-    {
-        seg[index]=left;
-    }
-    else
-    {
-        seg[index]=right;
-    }
-}
-int maxquery(int l,int r,int ql,int qr,int index,vector<int>&heights)
-{
-    if(qr<l||r<ql)
-    {
-        return INT_MIN;
-    }
-    if(ql<=l&&r<=qr)
-    {
-        return seg[index];
-    }
-    int mid=(l+r)/2;
-    int left=maxquery(l,mid,ql,qr,2*index+1,heights);
-    int right=maxquery(mid+1,r,ql,qr,2*index+2,heights);
-    if(left==INT_MIN)
-    {
-        return right;
-    }
-    if(right==INT_MIN)
-    {
-        return left;
-    }
-    return heights[left]>=heights[right]?left:right;
-}
-    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
-        int n=heights.size();
-        seg.resize(4*n,0);
-        build(0,0,heights.size()-1,heights);
+    vector<int> leftmostBuildingQueries(vector<int>& nums, vector<vector<int>>& queries) {
         vector<int>result;
-        for(int i=0;i<queries.size();i++)
+        st *s=new st();
+        s->build(s->root,0,nums.size()-1,nums);
+        for(auto q:queries)
         {
-            int a=queries[i][0];
-            int b=queries[i][1];
-            if(a>b)
-            {
-                swap(a,b);
-            }
-            int l=b+1;
-            int r=heights.size()-1;
-            if(heights[b]>heights[a])
-            {
-                result.push_back(b);
-                continue;
-            }
+            int a=q[0];
+            int b=q[1];
             if(a==b)
             {
                 result.push_back(a);
                 continue;
             }
+            if(a>b)
+            {
+                swap(a,b);
+            }
+            if(nums[b]>nums[a])
+            {
+                result.push_back(b);
+                continue;
+            }
+            int l=b+1;
+            int r=nums.size()-1;
             int ans=-1;
             while(l<=r)
             {
                 int mid=(l+r)/2;
-                int x=maxquery(0,heights.size()-1,l,mid,0,heights);
-                if(x!=INT_MIN&&heights[a]<heights[x]&&heights[b]<heights[x])
+                int maxi=s->query(s->root,0,nums.size()-1,b+1,mid);
+                if(maxi>nums[a]&&maxi>nums[b])
                 {
-                    ans=x;
+                    ans=mid;
                     r=mid-1;
                 }
                 else
